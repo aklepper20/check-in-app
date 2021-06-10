ae5de00365951ae7b72fab7a4d304e8089502204
Aly Klepper <alyklepper1@gmail.com>
2021-06-09 19:58:15 -0700

    adds edit/delete btn to renderentry

--- src/RenderList.js
+++ src/RenderList.js
@@ -8,31 +8,27 @@ const RenderList = (props) => {
     <div className="render-list">
       {
         props.posts.map((item) =>
-      <div className="card-container">
+      <div key={item.id} className="card-container">
         <div className="card">
-
-              <div>{item.dropdown}</div>
-
-              <div className="render-message">
-                <h4>{item.message}</h4>
-              </div>
-
-              <div className="render-headwinds">
-                <span>+</span>
-                <div>
-                  {item.headwinds}
-                </div>
-              </div>
-
-              <div className="render-tailwinds">
-                <span>-</span>
-                <div>
-                  {item.tailwinds}
-                </div>
-              </div>
-
-              <div>{id}</div>
-
+          <div>{item.dropdown}</div>
+          <div className="render-message">
+            <h4>{item.message}</h4>
+          </div>
+          <div className="render-headwinds">
+            <span>+</span>
+            <div>
+              {item.headwinds}
+            </div>
+          </div>
+          <div className="render-tailwinds">
+            <span>-</span>
+            <div>
+              {item.tailwinds}
+            </div>
+          </div>
+          <div>{id}</div>
+        <button>Edit</button>
+        <button>Delete</button>
         </div>
       </div>
         )
